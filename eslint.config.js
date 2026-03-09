2b6aa361d2a6dadd8d711c83dfa90279226e7ff7
Vladislav <v.suvorkin2017@gmail.com>
2026-03-09 16:34:34 +0300

    PMSYS-11 Проектирование FSD-архитектуры фронтенда

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import importPlugin from 'eslint-plugin-import';
+import reactPlugin from 'eslint-plugin-react';
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,5 +21,28 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      import: importPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      "react/jsx-indent": [2, 4],
+      "import/prefer-default-export": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [
+        2,
+        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      ],
+      "import/no-unresolved": "off",
+      "import/extensions": "off",
+      "no-unused-vars": "warn",
+      "react/require-default-props": "off",
+      "react/function-component-definition": "off",
+      "react/jsx-props-no-spreading": "warn",
+      "no-shadow": "off",
+      "import/no-extraneous-dependencies": "warn",
+      "no-underscore-dangle": "off",
+      "max-len": ["error", { ignoreComments: true, code: 100 }],
+    },
   },
-])
+]);
