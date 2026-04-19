ec398a9cb1b1e7b91b7f6d83bd4063a1f33c5778
Vladislav <v.suvorkin2017@gmail.com>
2026-04-19 19:56:50 +0300

    PMSYS-47 Исправление ошибок фронтенда: убрал логирование перехватчика ответов и убрал базовый url

--- src/app/api/axiosInstance.ts
+++ src/app/api/axiosInstance.ts
@@ -1,20 +1,11 @@
 import axios from 'axios';
 import { StorageService } from '@/shared/utils/StorageService';
-import { API_BASE } from './config';
 
-export const api = axios.create({
-  baseURL: API_BASE,
-});
+export const api = axios.create();
 
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    const user = StorageService.getItem('user');
-    console.log('🔍 User from storage:', user);
-    console.log('🔑 Token:', user?.token);
-    console.log('📡 Request URL:', `${API_BASE}/products`);
-    console.log('📨 Headers:', { Authorization: `Bearer ${user?.token}` });
-    console.error('401 response data:', error.response?.data);
     const isAuthEndpoint = error.config?.url?.includes('/auth/');
     if (error.response?.status === 401 && !isAuthEndpoint) {
       StorageService.removeItem('user');
